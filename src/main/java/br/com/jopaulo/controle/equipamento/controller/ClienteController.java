95840e554aa2d3b909a169fd5d44c2da1656600e
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2020-12-09 23:42:14 -0300

    Adicionando a anotação @RestController na classe controller

--- src/main/java/br/com/jopaulo/controle/equipamento/controller/ClienteController.java
+++ src/main/java/br/com/jopaulo/controle/equipamento/controller/ClienteController.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,12 +15,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.jopaulo.controle.equipamento.model.Cliente;
 import br.com.jopaulo.controle.equipamento.repository.ClienteRepository;
 
-@Controller
+@RestController
 public class ClienteController {
 	
 	@Autowired
